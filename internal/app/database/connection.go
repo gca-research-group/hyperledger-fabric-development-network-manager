c1707658c1a5a279a434eac677f5eb25ead204d4
Mailson Teles Borges <mailson.borges@sou.unijui.edu.br>
2025-02-25 16:57:09 -0300

    Add docker configuration

--- internal/app/database/connection.go
+++ internal/app/database/connection.go
@@ -2,17 +2,18 @@ package database
 
 import (
 	"fmt"
+	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func Connection() *gorm.DB {
-	host := "localhost"
-	port := "5432"
-	user := "postgres"
-	password := "JbeWkB98P2xf"
-	dbname := "hfndmdb"
+	host := os.Getenv("DATABASE_HOST")
+	port := os.Getenv("DATABASE_PORT")
+	user := os.Getenv("DATABASE_USER")
+	password := os.Getenv("DATABASE_PASSWORD")
+	dbname := os.Getenv("DATABASE_NAME")
 
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=UTC", host, user, password, dbname, port)
 
