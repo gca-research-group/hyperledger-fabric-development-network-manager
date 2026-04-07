6d490d554ea6c66d8b9317b7cc80e6989f2a12b0
Mailson Teles Borges <mailson.borges@sou.unijui.edu.br>
2026-04-07 15:11:20 -0300

    Add script to execute all samples

---
+++ .scripts/samples/network-deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+configs=(
+  "capability-v3_0"
+  "cross-version-compatibility"
+  "minimal-network"
+  "multi-channel-network-multi-consensus"
+  "multi-channel-network"
+  "multi-organisation-consortium"
+  "network-with-chaincode"
+)
+
+for config in "${configs[@]}"; do
+  echo "******* ${config} *******"
+  go run cmd/cli/main.go network deploy --config samples/${config}.yml
+done
